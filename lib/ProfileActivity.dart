8fbd82506d86d6ffa40c3fb9afd19c460c2a8cf5
subharadhe1234 <146325210+subharadhe1234@users.noreply.github.com>
2024-03-06 01:12:16 +0530

    signup &Login page make (4)

--- lib/ProfileActivity.dart
+++ lib/ProfileActivity.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ProfileActivity extends StatefulWidget {
   const ProfileActivity({super.key});
@@ -18,8 +19,6 @@ class _ProfileActivityState extends State<ProfileActivity> {
         height: double.maxFinite,
         child: Stack(
           children: [
-
-
             //bckground contener its content background color     ########################################
 
             Container(
@@ -35,35 +34,220 @@ class _ProfileActivityState extends State<ProfileActivity> {
                         begin: AlignmentDirectional(1, 0),
                         end: AlignmentDirectional(0, 1)))),
 
-
-                Column(
-                  children: [
-                    Stack(
-                      children: [ Container(
-                        width: double.maxFinite,
-                        height: 70,
+            Column(
+              children: [
+                Stack(children: [
+                  Container(
+                      width: double.maxFinite,
+                      height: 90,
                       // color: Colors.yellowAccent,
-                      
-                       child:  Stack(children: [ Positioned(
-                           bottom:0,left: 8,
-                           child: Text("PROFILE",style: TextStyle(color: Colors.red.shade900,fontSize: 35,fontWeight: FontWeight.w900),)),
-                       ])
 
-                       ),
-
-                    ]),
-            Divider(
-              color: Colors.black,
-              thickness: 2,
-              height: 0,
-            )
-                  ],
+                      child: Stack(
+                        children: [
+                          Positioned(
+                              bottom: 0,
+                              left: 8,
+                              child: Text(
+                                "PROFILE",
+                                style: TextStyle(
+                                    color: Colors.red.shade900,
+                                    fontSize: 35,
+                                    fontWeight: FontWeight.w900),
+                              )),
+                          Positioned(
+                              right: 10,
+                              bottom: 10,
+                              child: FaIcon(
+                                FontAwesomeIcons.notesMedical,
+                                size: 30,
+                              ))
+                        ],
+                      )),
+                ]),
+                Divider(
+                  color: Colors.black,
+                  thickness: 2,
+                  height: 0,
+                ),
+                CircleAvatar(
+                  maxRadius: 85,
+                  backgroundColor: Colors.cyan.withOpacity(0.0),
+                  child: Icon(
+                    Icons.account_circle_outlined,
+                    size: 180,
+                    color: CupertinoColors.black,
+                    fill: .5,
+                  ),
+                ),
+                Text(
+                  "Radhe Krishna",
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                ),
+                Text(
+                  "hareKrishna@gamil.com",
+                  style: TextStyle(color: Colors.black, fontSize: 15),
+                ),
+                SizedBox(
+                  height: 100,
+                ),
+                
+                ////  personal information
+                Container(
+                  width: double.maxFinite,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.circular(
+                        40,
+                      ),
+                      border: Border.all(color: Colors.black, width: 2)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 19.0),
+                    child: Stack(
+                      alignment: AlignmentDirectional.centerStart,
+                      children: [
+                        Text(
+                          "Personal Information",
+                          style: TextStyle(color: Colors.black, fontSize: 20),
+                        ),
+                        Positioned(
+                            right: 0,
+                            child: Icon(
+                              Icons.navigate_next,
+                              size: 60,
+                            ))
+                      ],
+                    ),
+                  ),
+                ),
+                
+                //time spend ##########
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: double.maxFinite,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.circular(
+                        40,
+                      ),
+                      border: Border.all(color: Colors.black, width: 2)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 19.0),
+                    child: Stack(
+                      alignment: AlignmentDirectional.centerStart,
+                      children: [
+                        Text(
+                          "Time Spend",
+                          style: TextStyle(color: Colors.black, fontSize: 20),
+                        ),
+                        Positioned(
+                            right: 0,
+                            child: Icon(
+                              Icons.navigate_next,
+                              size: 60,
+                            ))
+                      ],
+                    ),
+                  ),
+                ),
+                
+                //Personal Stats  #######
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: double.maxFinite,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.circular(
+                        40,
+                      ),
+                      border: Border.all(color: Colors.black, width: 2)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 19.0),
+                    child: Stack(
+                      alignment: AlignmentDirectional.centerStart,
+                      children: [
+                        Text(
+                          "Personal Stats",
+                          style: TextStyle(color: Colors.black, fontSize: 20),
+                        ),
+                        Positioned(
+                            right: 0,
+                            child: Icon(
+                              Icons.navigate_next,
+                              size: 60,
+                            ))
+                      ],
+                    ),
+                  ),
+                ),
+                
+                
+                //poind Gained
+                
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: double.maxFinite,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.circular(
+                        40,
+                      ),
+                      border: Border.all(color: Colors.black, width: 2)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 19.0),
+                    child: Stack(
+                      alignment: AlignmentDirectional.centerStart,
+                      children: [
+                        Text(
+                          "Point Gained",
+                          style: TextStyle(color: Colors.black, fontSize: 20),
+                        ),
+                        Positioned(
+                            right: 0,
+                            child: Icon(
+                              Icons.navigate_next,
+                              size: 60,
+                            ))
+                      ],
+                    ),
+                  ),
                 ),
 
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: double.maxFinite,
+
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 15.0),
+                      child: Row(
+                        children: [
+                          Text("Logout",style: TextStyle(color: Colors.red,fontSize: 20),),
+                          SizedBox(width: 10,),
+                          FaIcon(FontAwesomeIcons.arrowRightFromBracket,color: Colors.red,size: 30,shadows: [
+                            Shadow(color: Colors.purpleAccent.shade100,blurRadius: 10),
 
 
 
+                          ],),
 
+                        ],
+                      ),
+                    ),
+
+                )
+              ],
+            ),
           ],
         ),
       ),

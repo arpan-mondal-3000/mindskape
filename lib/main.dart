4642e58e05467203153abb2ce7116881eb240292
Arpan Mondal <139039003+arpan-mondal-3000@users.noreply.github.com>
2024-03-09 23:18:47 +0530

    categories page design complete

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
         ),
         debugShowCheckedModeBanner: false,
-        home: HomePages());
+        home: CategoriesScreen());
   }
 }
 
--- lib/screens/navigation_pages/categories/categoriesScreen.dart
+++ lib/screens/navigation_pages/categories/categoriesScreen.dart
@@ -20,19 +20,161 @@ class CategoriesScreen extends StatelessWidget {
                     opacity: .8)),
           ),
           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Container(
-                  padding: EdgeInsets.symmetric(vertical: mq.height*.05, horizontal: 20),
-                  child: Text(
+                  padding: EdgeInsets.fromLTRB(20, mq.height*.05, 0, 0),
+                  child: const Text(
                       'CATEGORIES',
                       style: TextStyle(
                         fontSize: 26,
+                        fontWeight: FontWeight.w800,
+                        color: Color(0xff6C464E),
                       ))),
+              const Divider(
+                color: Colors.black,
+                thickness: 2,
+                height: 0,
+              ),
+              Column(
+                children: [
+                  const SizedBox(height: 50,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        alignment: Alignment.center,
+                        width: 150,
+                        height: 150,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.white.withOpacity(0.7),
+                        ),
+                        child: const Text(
+                          'MOOD TRACKER',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 24,
+                            color: Color(0xff6C464E),
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 20,),
+                      Container(
+                        alignment: Alignment.center,
+                        width: 150,
+                        height: 150,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.white.withOpacity(0.7),
+                        ),
+                        child: const Text(
+                          'PERSONAL TIPS',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 24,
+                            color: Color(0xff6C464E),
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  const SizedBox(height: 20,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        alignment: Alignment.center,
+                        width: 150,
+                        height: 150,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.white.withOpacity(0.7),
+                        ),
+                        child: const Text(
+                          'MENTAL HEALTH EDUCATION',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 24,
+                            color: Color(0xff6C464E),
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 20,),
+                      Container(
+                        alignment: Alignment.center,
+                        width: 150,
+                        height: 150,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.white.withOpacity(0.7),
+                        ),
+                        child: const Text(
+                          'NEWS',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 24,
+                            color: Color(0xff6C464E),
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  const SizedBox(height: 20,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        alignment: Alignment.center,
+                        width: 150,
+                        height: 150,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.white.withOpacity(0.7),
+                        ),
+                        child: const Text(
+                          'JOURNAL',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 24,
+                            color: Color(0xff6C464E),
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 20,),
+                      Container(
+                        alignment: Alignment.center,
+                        width: 150,
+                        height: 150,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.white.withOpacity(0.7),
+                        ),
+                        child: const Text(
+                          'FEEDBACK',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 24,
+                            color: Color(0xff6C464E),
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )
             ],
           )
         ],
       ),
-
     );
   }
 }
